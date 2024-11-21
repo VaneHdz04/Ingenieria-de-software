f8e6fda8d6a040bfb83221a7cbefb2766b45a69b
VaneHdz04 <vanehdzmtz04@gmail.com>
2024-11-21 10:30:00 -0600

    ejercicio2

---
+++ ejercicio2.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 2</title>
+</head>
+<body>
+    <?php
+        $edad = 20;
+        $peso = 90.25;
+        $nombre = "Juan";
+        $existe = true;
+        echo "Variable integer: ";
+        echo $edad;
+        echo "<br>";
+        echo "Variable double: ";
+        echo $peso;
+        echo "<br>";
+        echo "Variable string: ";
+        echo $nombre;
+        echo "<br>";
+        echo "Variable boolean: ";
+        echo $existe;
+      
+    ?>
+</body>
+</html>
