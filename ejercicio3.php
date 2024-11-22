1427450b366b72095402452f0151828d5436d646
VaneHdz04 <vanehdzmtz04@gmail.com>
2024-11-22 10:30:00 -0600

    ejercicio3

---
+++ ejercicio3.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 3</title>
+</head>
+<body>
+    <?php
+    // Se concatena con .
+    // Despejar variables
+
+    $nota1 = 10;
+    $nota2 = 7;
+    $nota3 = 8;
+    echo "Juan aprobó la materia con las notas $nota1, $nota2 y $nota3";
+    ?>
+</body>
+</html>
