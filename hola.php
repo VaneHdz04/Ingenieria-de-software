f45e10f24eb23a0fe2202773abb707f1d76a6714
VaneHdz04 <vanehdzmtz04@gmail.com>
2024-11-26 10:30:00 -0600

    hola

---
+++ hola.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hola Mundo con PHP</title>
+</head>
+<body>
+    <h1>
+        <?php
+        // Este es un comentario en PHP
+        echo "¡Hola, mundo!";
+        ?>
+    </h1>
+</body>
+</html>
