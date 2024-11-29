47ec2ac99fd9eb06aef0be00fb4b51ba95d48bf5
VaneHdz04 <vanehdzmtz04@gmail.com>
2024-11-29 10:30:00 -0600

    pag1

---
+++ pag1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 8</title>
+</head>
+<body>
+    <form method="post" action="pag2.php">
+        Ingrese su nombre:
+        <input type="text" name="nombre">
+        <br>
+        <input type="submit" value="confirmar">
+    </form>
+  
+</body>
+</html>
