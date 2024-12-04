5f97a13d8c631a6332c9eb6711fa3a7ef6a58125
VaneHdz04 <vanehdzmtz04@gmail.com>
2024-12-04 10:30:00 -0600

    pagina1_1

---
+++ pagina1_1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina 1 PHP</title>
+</head>
+<body>
+    <form action="pagina2.php" meth="post">
+        Ingrese primer valor:
+        <input type="text" name="valor1">
+        <br>
+        Ingrese segundo valor:
+        <input type="text" name="valor2">
+        <br>
+        <input type="radio" name="radio1" value="suma">sumar
+        <br>
+        <input type="radio" name="radio1" value="resta">resta
+        <br>
+        <input type = "submit" name="operar">
+    </form>
+</body>
+</html>
